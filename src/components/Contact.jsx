b760d7f8cb81c2559fbe858a35554f25f8c27642
sidharthsunil1 <ss9400352506@gmail.com>
2025-02-16 23:56:55 +0530

    hhi

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,1 +1,12 @@
-dakjdawld
+import React from 'react';
+
+const Contact = () => {
+  return (
+	<div>
+	  <h1>Contact Us</h1>
+	  <p>Please reach out to us at contact@example.com</p>
+	</div>
+  );
+};
+
+export default Contact;
